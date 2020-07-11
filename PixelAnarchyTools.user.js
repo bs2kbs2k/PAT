635834164794bea515a2c6c4d0a77c011633e952
bs2kbs2k <bs2k@naver.com>
2020-07-11 18:03:22 +0900

    Create PixelAnarchyTools.user.js

---
+++ PixelAnarchyTools.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           Pixel Anarchy Tools
+// @namespace      pixel_anarchy_tools
+// @description    An Image overlay for the site Pixel Anarchy Online to make fixing griefed areas easier
+// @include        http://*.pixelanarchy.online/*
+// @include        https://pixelanarchy.online/*
+// @match          http://pixelanarchy.online/*
+// @match          https://pixelanarchy.online/*
+// @version        0.3
+// ==/UserScript==
+
+document.getElementById('brushsize2').parentElement.children[10].insertAdjacentHTML('afterend','<div style="color: white;"> <h2>Opacity</h2> <input type="range" min="1" max="100" value="50" class="slider" id="opacity" onchange="document.getElementById(\'overlay\').style.opacity = document.getElementById(\'opacity\').value/100"> <br> <h2>Overlay URL</h2> <input id="url" style="width: 90%; height: 5%;" autocomplete="off" onchange="document.getElementById(\'overlay\').src = document.getElementById(\'url\').value"> <br> <h2>Overlay Position</h2> <input type="number" maxlength="100" id="x" style="width: 90%; height: 5%;" autocomplete="off" min="0" value="0" step="1" max="5759" onchange="document.getElementById(\'overlay\').style.left = document.getElementById(\'x\').value+\'px\'"> <input type="number" maxlength="100" id="y" style="width: 90%; height: 5%;" autocomplete="off" min="0" value="0" step="1" max="3239" onchange="document.getElementById(\'overlay\').style.top = document.getElementById(\'y\').value-3240+\'px\'"> <br> <h2>Overlay Scale</h2> <input type="number" maxlength="100" id="scale" style="width: 90%; height: 5%;" autocomplete="off" min="0" value="1" onchange="document.getElementById(\'overlay\').style.width = document.getElementById(\'scale\').value+\'px\'"> <br> <h2>Grid</h2> <input id="gridToggle" type="checkbox" onchange="document.getElementById(\'grid\').style.display = document.getElementById(\'gridToggle\').checked ? \'unset\':\'none\'" checked> Use Grid <br> </div>');
+document.getElementById('myCanvas').insertAdjacentHTML('afterend','<img style="pointer-events: none; position: absolute; top: 0; left: 0; image-rendering: pixelated;" src="https://gist.githubusercontent.com/bs2kbs2k/8158efdeee8b08c31bf0915b32f9a74d/raw/f544064d16439f682a03ea3077ae037fc167e7e1/grid.svg" id="grid">');
+document.getElementById('myCanvas').insertAdjacentHTML('afterend','<img style="pointer-events: none; position: relative; top: -3240px; left: 0px; opacity: 0.5; image-rendering: pixelated; transform: scale(1);" src="" id="overlay">');
