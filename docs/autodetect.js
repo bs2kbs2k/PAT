adbfacf25b328654047d09d3c07464eb3c14fe67
bs2kbs2k <bs2k@naver.com>
2020-07-18 16:50:41 +0900

    Update autodetect.js

--- docs/autodetect.js
+++ docs/autodetect.js
@@ -8,18 +8,19 @@ if(navigator.userAgent.includes("Edg/")){
 }
 
 let elem = document.getElementById("dl");
+let link = document.getElementById("dl_lnk");
 switch(browser){
     case "newEdge":
         elem.src = "https://img.shields.io/badge/download-Violentmonkey-blue";
-        elem.href = "https://microsoftedge.microsoft.com/addons/detail/eeagobfjdenkkddmbclomhiblgggliao";
+        link.href = "https://microsoftedge.microsoft.com/addons/detail/eeagobfjdenkkddmbclomhiblgggliao";
         break;
     case "chromiumBased":
         elem.src = "https://img.shields.io/badge/download-Violentmonkey-blue";
-        elem.href = "https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag";
+        link.href = "https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag";
         break;
     case "firefox":
         elem.src = "https://img.shields.io/badge/download-Violentmonkey-blue";
-        elem.href = "https://addons.mozilla.org/firefox/addon/violentmonkey/";
+        link.href = "https://addons.mozilla.org/firefox/addon/violentmonkey/";
         break;
     case "unsupported":
         elem.src = "https://img.shields.io/badge/unsupported-browser-red";
